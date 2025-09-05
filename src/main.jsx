548f43e82683f7b2486059619bb79d25da528702
Sarthak Gupta <136185773+SkG999@users.noreply.github.com>
2025-09-05 23:25:58 +0530

    Update main.jsx
    Changed main.jsx's server = from localhost to github server

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 //import './index.css'
-export const server = "http://localhost:5000";
+export const server = "https://learnify-backend-2.onrender.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserContextProvider>
